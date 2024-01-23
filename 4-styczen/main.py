89cbef922a288767d81755aead7d51efd25bd911
F4s0lix <f4s0lix@protonmail.com>
2024-01-23 17:34:22 +0100

    dodano wypisywanie zadania 3.1 FIXME: wypisuje tylko jedną wartość a powinno kilka, jeżeli mają takie same długości SOLVE:zamiast .index użyć czegoś innego

--- 4-styczen/main.py
+++ 4-styczen/main.py
@@ -64,7 +64,12 @@ def daty_temperatura() -> str:
     return odp
 
 print('=' * 30 + ' zadanie 3.1 ' + '=' * 30)
-
+wynik: list = daty_temperatura()
+wynik_dlugosci = list(map(lambda x: len(x), wynik))
+najwieksza_index = wynik_dlugosci.index(max(wynik_dlugosci))
+print(wynik[najwieksza_index][0][0] + ' - ' + wynik[najwieksza_index][-1][0])
+#FIXME: wypisuje jeden a powinien kilka największych - do naprawy
+print('=' * 73)
 print('=' * 30 + ' zadanie 3.3 ' + '=' * 30)
 print(ile_ponizej_srednia())
 print('=' * 73)
